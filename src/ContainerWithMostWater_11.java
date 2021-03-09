cc335f61da7c0b7612819e61177bda65649687f8
zqh1059405318 <1059405318@qq.com>
2021-03-09 21:35:30 +0800

    补了之前的几道题

---
+++ src/ContainerWithMostWater_11.java
@@ -0,0 +1,18 @@
+public class ContainerWithMostWater_11 {
+    //高由较低的一个决定
+    public int maxArea(int[] height) {
+        int tail = height.length - 1;
+        int head = 0;
+        int maxSize = 0;
+        while (head < tail) {
+            int curSize = (tail - head) * Math.min(height[head], height[tail]);
+            maxSize = Math.max(maxSize, curSize);
+            if (height[head] > height[tail]) {
+                tail -= 1;
+            } else {
+                head += 1;
+            }
+        }
+        return maxSize;
+    }
+}
---
+++ src/ThreeSumClosest_16.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class ThreeSumClosest_16 {
+/*    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int len = nums.length;
+    }*/
+}
