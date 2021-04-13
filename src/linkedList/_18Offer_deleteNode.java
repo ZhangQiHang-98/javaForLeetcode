9042a562e23ea9a609ed27f739bf853b86eb3fe2
zqh1059405318 <1059405318@qq.com>
2021-04-13 20:14:38 +0800

    4.13,添加了一些注释

--- src/linkedList/_18Offer_deleteNode.java
+++ src/linkedList/_18Offer_deleteNode.java
@@ -1,5 +1,6 @@
 package linkedList;
 
+// 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。返回删除后的链表的头节点。
 public class _18Offer_deleteNode {
     public class ListNode {
         int val;
@@ -21,16 +22,16 @@ public class _18Offer_deleteNode {
     public ListNode deleteNode(ListNode head, int val) {
         ListNode dummy = new ListNode();
         dummy.next = head;
-        // pre是当前index最大的不为val的值
+        // pre是当前index最大的不为val的值，pre永远存储的是latest的correct
         ListNode pre = dummy;
         ListNode cur = head;
         if (head == null) {
             return head;
         }
-        while (cur!=null){
-            if(cur.val == val){
+        while (cur != null) {
+            if (cur.val == val) {
                 pre.next = cur.next;
-            }else {
+            } else {
                 pre.next = cur;
                 pre = cur;
             }
