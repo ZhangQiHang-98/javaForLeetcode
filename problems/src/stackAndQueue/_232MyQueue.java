a036455fb6b78402ae23da5baeb3f1668d871b85
zqh1059405318 <1059405318@qq.com>
2021-06-05 10:38:40 +0800

    add .gitignore

---
+++ problems/src/stackAndQueue/_232MyQueue.java
@@ -0,0 +1,44 @@
+package stackAndQueue;
+
+import java.util.Stack;
+
+public class _232MyQueue {
+
+    /**
+     * Initialize your data structure here.
+     */
+    Stack<Integer> sIn = new Stack<>();
+    Stack<Integer> sOut = new Stack<>();
+
+    public MyQueue() {
+
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+
+    }
+}
